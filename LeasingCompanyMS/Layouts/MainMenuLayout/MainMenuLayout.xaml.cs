d2074806b88a57d03105ab50a00cd864ad521886
Michał Jasiński <mcjasiux@gmail.com>
2025-01-31 22:11:32 +0100

    logout button

--- LeasingCompanyMS/Layouts/MainMenuLayout/MainMenuLayout.xaml.cs
+++ LeasingCompanyMS/Layouts/MainMenuLayout/MainMenuLayout.xaml.cs
@@ -1,7 +1,19 @@
-﻿namespace LeasingCompanyMS.Layouts.MainMenuLayout;
+﻿using System.Security.Principal;
+using System.Windows;
+using LeasingCompanyMS.Components.MainWindow;
+using LeasingCompanyMS.Model;
+using LeasingCompanyMS.Pages.LoginPage;
+
+namespace LeasingCompanyMS.Layouts.MainMenuLayout;
 
 public partial class MainMenuLayout {
     public MainMenuLayout() {
         InitializeComponent();
+    }
+
+    private void LogoutAction(object sender, RoutedEventArgs e)
+    {
+        Thread.CurrentPrincipal = null;
+        MainWindow.NavigationService.Navigate(new LoginPage());
     }
 }
